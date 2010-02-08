ec7171779adc47246ff61f0120ef4fb9c4503a6a
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-08 15:17:34 +0000

    upload to ter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/js_css_optimizer/tags/1.0.1@29814 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/hooks/class.tx_js_css_optimizer_hooks.php
+++ classes/hooks/class.tx_js_css_optimizer_hooks.php
@@ -84,12 +84,11 @@ abstract class tx_js_css_optimizer_hooks {
 	 * @return string
 	 */
 	protected function fixRelativeCssPaths($baseFolder, $content){
-		if(substr($baseFolder,0,1) !== '/'){
-			$baseFolder = '/'.$baseFolder;
-		}
-		$content =  preg_replace('/url[ ]*\([ ]*[\.\.\/]{3}([\w]+\.[\w]+)/i', 'url('.$baseFolder.'/$1', $content ); // background: url(../test3.gif);
-		$content =  preg_replace('/url[ ]*\([ ]*([a-z|0-9|_|-]+)/i', 'url('.$baseFolder.'/$1', $content ); // background: url(images/test2.gif); 
-		$content =  preg_replace('/url[ ]*\([ ]*([\.\.\/]{3})([\.\.\/]*)/i', 'url('.$baseFolder.'/$1$2', $content ); //background: url(../images/test1.gif);  or background: url(../../images/test4.gif);
+		$baseFolder = t3lib_div::getIndpEnv('TYPO3_SITE_PATH').$baseFolder; 
+		$content =  preg_replace('/url[ ]*\([ ]*[\']*[\.\.\/]{3}([\w]+\.[\w]+[\']*)/i', 'url('.$baseFolder.'/$1', $content ); // background: url(../test3.gif);
+		
+		$content =  preg_replace('/url[ ]*\([ ]*[\']*([a-z|0-9|_|-]+[\']*)/i', 'url('.$baseFolder.'/$1', $content ); // background: url(images/test2.gif); 
+		$content =  preg_replace('/url[ ]*\([ ]*[\']*([\.\.\/]{3})([\.\.\/]*[\']*)/i', 'url('.$baseFolder.'/$1$2', $content ); //background: url(../images/test1.gif);  or background: url(../../images/test4.gif);
 		return $content;
 	}
 }
