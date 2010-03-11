20ed1eda375a1a1a7391ce7c9a923d512241f989
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-11 10:55:36 +0000

    do not prozess empty root path
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/js_css_optimizer/trunk@31006 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/hooks/class.tx_js_css_optimizer_hooks.php
+++ classes/hooks/class.tx_js_css_optimizer_hooks.php
@@ -84,7 +84,11 @@ abstract class tx_js_css_optimizer_hooks {
 	 * @return string
 	 */
 	protected function fixRelativeCssPaths($baseFolder, $content){
-		$baseFolder = t3lib_div::getIndpEnv('TYPO3_SITE_PATH').$baseFolder; 
+		$root = t3lib_div::getIndpEnv('TYPO3_SITE_PATH');
+		if(empty($root)){
+			throw new Exception('could not find root path');
+		}
+		$baseFolder = $root.$baseFolder; 
 		$content =  preg_replace('/url[ ]*\([ ]*[\']*[\.\.\/]{3}([\w]+\.[\w]+)[\']*/i', 'url('.$baseFolder.'/$1', $content ); // background: url(../test3.gif);
 		$content =  preg_replace('/url[ ]*\([ ]*[\']*([a-z|0-9|_|-]+)[\']*/i', 'url('.$baseFolder.'/$1', $content ); // background: url(images/test2.gif); 
 		$content =  preg_replace('/url[ ]*\([ ]*[\']*([\.\.\/]{3})([\.\.\/]*)[\']*/i', 'url('.$baseFolder.'/$1$2', $content ); //background: url(../images/test1.gif);  or background: url(../../images/test4.gif);
