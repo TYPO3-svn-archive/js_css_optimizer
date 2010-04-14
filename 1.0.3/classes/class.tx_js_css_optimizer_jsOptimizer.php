8de8d07e39cdea81e99bd4601748a2d2470508cc
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-14 13:18:34 +0000

    new version in ter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/js_css_optimizer/tags@32208 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ 1.0.3/classes/class.tx_js_css_optimizer_jsOptimizer.php
@@ -0,0 +1,41 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2009 AOE media GmbH <dev@aoemedia.de>
+ *  All rights reserved
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+require_once PATH_site.TYPO3_mainDir.'contrib'.DIRECTORY_SEPARATOR .'jsmin'.DIRECTORY_SEPARATOR.'jsmin.php';
+/**
+ *	js compressor
+ */
+class tx_js_css_optimizer_jsOptimizer {
+	/**
+	 * 
+	 * @var tx_js_css_optimizer_jsOptimizer
+	 */
+	private static $instance = null;
+	/**
+	 * contructor
+	 */
+	private function __construct() {}
+	/**
+	 * @return tx_js_css_optimizer_jsOptimizer
+	 */
+	static public function getInstance() {
+		if (null === self::$instance) {
+			self::$instance = new self ( );
+		}
+		return self::$instance;
+	}
+	/**
+	 * @param string $content
+	 * @return string
+	 */
+	public function compress($content) {
+		$errorMessage = null;
+		return JSMin::minify ($content,$errorMessage);
+	}
+}
