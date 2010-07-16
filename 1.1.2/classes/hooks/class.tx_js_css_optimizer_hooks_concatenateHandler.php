4b3f2880ca02ad8fa697f5b06283f28820462009
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-16 09:01:24 +0000

    new version in ter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/js_css_optimizer/tags@35737 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ 1.1.2/classes/hooks/class.tx_js_css_optimizer_hooks_concatenateHandler.php
@@ -0,0 +1,127 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2009 AOE media GmbH <dev@aoemedia.de>
+ *  All rights reserved
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+require_once (dirname ( __FILE__ ) . DIRECTORY_SEPARATOR . 'class.tx_js_css_optimizer_hooks.php');
+/**
+ * Hook to bundle the js and css files
+ * @package js_css_optimizer
+ */
+class tx_js_css_optimizer_hooks_concatenateHandler extends tx_js_css_optimizer_hooks {
+	/**
+	 * @param array $args
+	 * @return void
+	 */
+	public function process(array $args) {
+		$conf = unserialize ( $GLOBALS ['TYPO3_CONF_VARS'] ['EXT'] ['extConf'] ['js_css_optimizer'] );
+		if($conf ['bundle_js'] ){
+			if (count ( $args ['jsLibs'] ) > 0) {
+				$this->createNewJsLibBundle ( $args ['jsLibs'], '_bundled_jsLibs.js' );
+			}
+			if (count ( $args ['jsFiles'] ) > 0) {
+				$this->createNewJsBundle ( $args ['jsFiles'], '_bundled_jsFiles.js');
+			}
+			if (count ( $args ['jsFooterFiles'] ) > 0) {
+				$this->createNewJsBundle ( $args ['jsFooterFiles'], '_bundled_jsFooterFiles.js' );
+			}
+		}
+		if($conf ['bundle_css']){
+			if (count ( $args ['cssFiles'] ) > 0) {
+				$charsetCSS = null;
+				if(isset($conf ['charsetCSS']) && !empty($conf ['charsetCSS'])){
+					$charsetCSS = $conf ['charsetCSS'];
+				}
+				$this->createNewCssBundle ( $args ['cssFiles'], '_bundled_cssFiles.css',$charsetCSS );
+			}
+		}
+	}
+	/**
+	 * @param array &$files
+	 * @param string $filename
+	 * @return string
+	 */
+	private function getFileName(array &$files, $filename){
+		return sha1 ( serialize ( $files ) ) . $filename;
+	}
+	/**
+	 * @param array &$files
+	 * @param string $filename
+	 * @return void
+	 */
+	private function createNewJsLibBundle(array &$files, $filename) {
+		$topContent = '';
+		$content = '';
+		$jsFilesOnBottom = array();
+		foreach ( $files as $name => $meta ) {
+			if(empty($meta['allWrap'])){
+				$filecontent = $this->getFileContent ( $meta ['file'] );
+				if($meta['forceOnTop']){
+					$topContent .= $filecontent;
+				}else{
+					$content .= $filecontent;
+				}
+				unset ( $files [$name] );
+			}else{
+				if(!$meta['forceOnTop']){
+					$jsFilesOnBottom[$name] = $files [$name] ;
+					unset ( $files [$name] );
+				}
+			}
+		}
+		$content = $topContent.$content;
+		$newFile = $this->createCacheFile ( sha1($content).$filename, $content );
+		$files ['bundledLib'] = array ('file' => $newFile, 'type' => 'text/javascript', 'section' => t3lib_PageRenderer::PART_HEADER, 'compressed' => false, 'forceOnTop' => false, 'allWrap' => '' );
+		foreach($jsFilesOnBottom as $name => $meta){
+			$files [$name] = $meta;
+		}
+	}
+	/**
+	 * @param array &$files
+	 * @param string $filename
+	 * @param string $charsetCSS
+	 * @return void
+	 */
+	private function createNewCssBundle(array &$files, $filename,$charsetCSS = null) {
+		$content = '';
+		foreach ( $files as $file => $meta ) {
+			$filecontent = $this->getFileContent ( $file );
+			$content .= $this->fixRelativeCssPaths(dirname($file),$filecontent);
+			unset ( $files [$file] );
+		}
+		$matches = array();
+		if(preg_match_all('/@charset.*?;/i',$content,$matches)){
+			if(count($matches[0])>1 || FALSE === is_null($charsetCSS)){
+				foreach($matches[0] as $match){
+					$content = str_replace($match,'',$content);
+				}
+			}
+		}
+		if(!preg_match('/@charset.*;/i',$content) && FALSE === is_null($charsetCSS)){
+			$content = '@charset "'.$charsetCSS.'";'.PHP_EOL.$content;
+		}
+		
+		$newFile = $this->createCacheFile (sha1($content). $filename, $content );
+		$files [$newFile] = $meta;
+	}
+	
+	/**
+	 * @param array &$files
+	 * @param string $filename
+	 * @return void
+	 */
+	private function createNewJsBundle(array &$files, $filename) {
+		$content = '';
+		foreach ( $files as $file => $meta ) {
+			$filecontent = $this->getFileContent ( $file );
+			$content .= $filecontent;
+			unset ( $files [$file] );
+		}
+		$newFile = $this->createCacheFile ( sha1($content).$filename, $content );
+		$files [$newFile] = $meta;
+	}
+}
