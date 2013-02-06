e66df0ce6754d7b71e685eddcf338cb1facc9201
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-06 09:34:37 +0000

    Increase allowed TYPO3 version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/js_css_optimizer/trunk@70804 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
-			'typo3' => '4.3.0-4.5.99',
+			'typo3' => '4.3.0-4.7.99',
 		),
 		'conflicts' => array(
 		),
